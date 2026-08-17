3b3cda5d993d06d0d912087140a61c59c38e8c99
CARDONA LUIS <luis.cardona@aiondex.com>
2026-08-17 10:36:24 -0600

    publicacion de la app en play store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keyPropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.aiondex.aionstyle_movil"
+    namespace = "com.aionstyle.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.aiondex.aionstyle_movil"
+        applicationId = "com.aionstyle.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
